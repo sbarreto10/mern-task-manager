4e75397611e059d6483cb34887b7b250a6fa3bee
sbarreto10 <sbarreto@fi.uba.ar>
2023-10-17 19:54:26 -0300

    added styled box at the bottom of RegisterPage to display register errors
    (this should be on a separated component, working on that)

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -5,10 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 function RegisterPage() {
   // register
-  const {
-    register,
-    handleSubmit
-  } = useForm();
+  const { register, handleSubmit } = useForm();
   const { signup, isAuthenticated, errors: errors } = useAuth(); // Los valores del contexto
   const navigate = useNavigate();
 
@@ -29,29 +26,24 @@ function RegisterPage() {
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          {...register("username")}
-          placeholder="Username"
-        />
-        {errors.username && <span>{errors.username.message}</span>}
-        <input
-          type="email"
-          {...register("email")}
-          placeholder="Email"
-        />
-        {errors.email && <span>{errors.email.message}</span>}
+        <input type="text" {...register("username")} placeholder="Username" />
+        <input type="email" {...register("email")} placeholder="Email" />
         <input
           type="password"
           {...register("password")}
           placeholder="Password"
         />
-        {errors.password && 
-        <span>{errors.password.message}</span>
-        }
         <button type="submit">Sign in</button>
       </form>
-      
+      {Boolean(errors.length) && (
+        <div className="d-flex flex-column justify-content-center gap-2 p-2">
+          {errors.map((err) => (
+            <div className="text-danger" key={err}>
+              * {err}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
