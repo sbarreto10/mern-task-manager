d97334d9fff439310b6bf50f14fde5931f67cdaf
sbarreto10 <sbarreto@fi.uba.ar>
2023-10-17 20:02:50 -0300

    .

--- server/src/middlewares/validator.middleware.js
+++ server/src/middlewares/validator.middleware.js
@@ -1,7 +1,6 @@
 export const validateSchema = (schema) => (req, res, next) => {
    try {
      schema.parse(req.body);
-     console.log("si se pudo");
      next();
    } catch (error) {
      return res
