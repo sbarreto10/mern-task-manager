e4b99067b7d57dc0fcea4744bd9917b5c0c9f8ab
sbarreto10 <sbarreto@fi.uba.ar>
2023-11-07 16:51:37 -0300

    Add NavBar component into App
    Edit window title and favicon

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,12 @@
 function NavBar() {
   return (
     <div className="navbar navbar-expand-lg navbar-dark bg-dark p-4">
-      <div className="navbar-brand">{"TaskManager"}</div>
+      <a className="navbar-brand d-flex align-items-center gap-4" href="#">
+        <img src="./tskmngr.svg" className="w-25" alt="TaskManager Logo" />
+        <h1>{"TaskManager"}</h1>
+      </a>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
