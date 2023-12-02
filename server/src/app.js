3af0a2f86fd79ee3a5dc895d3856e029b386ab3b
sbarreto10 <sbarreto@fi.uba.ar>
2023-12-02 01:34:02 -0300

    Set up and handle CORS response headers

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,8 @@ import express from "express";
 import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/task.routes.js";
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import { config } from "dotenv";
 
 config();
@@ -12,19 +12,20 @@ const app = express();
 
 // HEALTH CHECK
 app.get("/okidoki", (req, res) => {
-   res.sendStatus(200)
- })
+   res.sendStatus(200);
+});
 
-app.use(cors({
-   origin: process.env.CORS_ORIGIN,
-   credentials: true
-}))
+app.use(
+   cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+   })
+);
 app.use(morgan("dev")); // para visualizar las peticiones
 app.use(express.json()); // para transformar los req.body en json (si no, el backend devolvería undefined)
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api", authRoutes); // todas las rutas de authRoutes empezaran con /api
 app.use("/api", taskRoutes);
-
 
 export default app;
--- server/src/middlewares/validateToken.js
+++ server/src/middlewares/validateToken.js
@@ -1,14 +1,27 @@
 import jwt from "jsonwebtoken";
 
 export const authRequired = (req, res, next) => {
-  const { token } = req.cookies;
+   res.header("Access-Control-Allow-Credentials", true);
+   res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);
+   res.header(
+      "Access-Control-Allow-Methods",
+      "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
+   );
+   res.header(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+   );
 
-  if (!token)
-    return res.status(401).json({ message: "No token, authorization denied" });
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, decodedUser) => {
-   if(err) return res.status(401).json({message: "Invalid token"})
-   req.user = decodedUser
-  });
+   const { token } = req.cookies;
 
-  next();
+   if (!token)
+      return res
+         .status(401)
+         .json({ message: "No token, authorization denied" });
+   jwt.verify(token, process.env.TOKEN_SECRET, (err, decodedUser) => {
+      if (err) return res.status(401).json({ message: "Invalid token" });
+      req.user = decodedUser;
+   });
+
+   next();
 };
