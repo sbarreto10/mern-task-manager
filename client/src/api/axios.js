0b361de8e0d8b968f7a752ea7e484e68d20df6f9
sbarreto10 <sbarreto@fi.uba.ar>
2023-12-02 02:42:57 -0300

    Add properties to axios config

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const axiosConfig = axios.create({
   baseURL: import.meta.env.VITE_AXIOS_BASE_URL,
   withCredentials: true,
+	headers: {'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
+  credentials: "include"
 });
 
 export default axiosConfig;
