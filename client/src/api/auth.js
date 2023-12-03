49fbc71c2e6a3734542dbe03fe938536bca4d260
sbarreto10 <sbarreto@fi.uba.ar>
2023-12-03 20:46:42 -0300

    Add user parameter to logout CRUD request

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,5 +1,5 @@
 import axios from "./axios.js"
 export const registerRequest = (user) => axios.post(`/register`, user)
 export const loginRequest = (user) => axios.post(`/login`, user)
-export const logoutRequest = () => axios.post(`/logout`)
+export const logoutRequest = (user) => axios.post(`/logout`)
 export const authRequest = () => axios.get(`/auth`)
